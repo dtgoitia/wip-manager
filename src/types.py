2f2935bdf605f9207c4a2b6ead5d9d00cdb8310c
David Torralba Goitia <david.torralba.goitia@gmail.com>
2021-09-01 14:14:46 +0100

    Parse multiple tasks

--- src/types.py
+++ src/types.py
@@ -11,6 +11,7 @@ COMPLETED_TASK_PREFIX = "- [x] "
 DETAIL_PREFIX = "- "
 _NO_DETAILS: List[TaskDetail] = []
 TASK_TAG_PATTERN = re.compile(r"\s#([a-z]:[a-z0-9-_,]*)\s?")  # `#g:group1_b`
+LINE_IS_TASK_PATTERN = re.compile(r"^- \[")  # starts with `- [ ] ` or `- [x] `
 
 
 @dataclass
@@ -21,7 +22,43 @@ class Task:
     tags: List[TaskTag]
 
 
-def parse(raw: MarkdownStr) -> Task:
+def parse(raw: MarkdownStr) -> List[Task]:
+    lines_buffer = []
+    parsed_tasks = []
+
+    lines = iter(raw.split("\n"))
+
+    lines_buffer.append(next(lines))
+    for line in lines:
+        line_is_task = is_task(line)
+
+        if line_is_task:
+            # Flush buffered lines
+            buffered_lines = "\n".join(lines_buffer)
+            task = parse_task(buffered_lines)
+            parsed_tasks.append(task)
+
+            # Replace line buffer with current line
+            lines_buffer = [line]
+        else:
+            # At this point you cannot know if the next line is a detail or another task
+            lines_buffer.append(line)
+
+    # Flush last bits in the buffer
+    buffered_lines = "\n".join(lines_buffer)
+    task = parse_task(buffered_lines)
+    parsed_tasks.append(task)
+
+    return parsed_tasks
+
+
+def is_task(raw_line: MarkdownStr) -> bool:
+    task_patterns_in_line = LINE_IS_TASK_PATTERN.match(raw_line)
+    is_task = bool(task_patterns_in_line)
+    return is_task
+
+
+def parse_task(raw: MarkdownStr) -> Task:
     task_has_details = "\n" in raw
     if task_has_details:
         description, raw_details = raw.split("\n", maxsplit=1)
--- tests/test_types.py
+++ tests/test_types.py
@@ -1,17 +1,17 @@
 import pytest
 
-from src.types import MarkdownStr, Task, parse
+from src.types import MarkdownStr, Task, parse, parse_task
 
 
 def test_parse_a_single_uncompleted_task():
     raw: MarkdownStr = "- [ ] Do foo"
-    task = parse(raw)
+    task = parse_task(raw)
     assert task == Task(description="Do foo", completed=False, details=[], tags=[])
 
 
 def test_parse_a_single_completed_task():
     raw: MarkdownStr = "- [x] Do foo"
-    task = parse(raw)
+    task = parse_task(raw)
     assert task == Task(description="Do foo", completed=True, details=[], tags=[])
 
 
@@ -22,7 +22,7 @@ def test_parse_a_single_uncompleted_task_with_details():
         "\n  - details line 2"
         "\n  - details line 3"
     )
-    task = parse(raw)
+    task = parse_task(raw)
     assert task == Task(
         description="Do foo",
         completed=False,
@@ -37,7 +37,7 @@ def test_parse_a_single_uncompleted_task_with_details():
 
 def test_parse_a_single_uncompleted_task_with_tags():
     raw: MarkdownStr = "- [ ] Do foo  #g:group1"
-    task = parse(raw)
+    task = parse_task(raw)
     assert task == Task(
         description="Do foo",  # do not show tags in the description
         completed=False,
@@ -46,9 +46,19 @@ def test_parse_a_single_uncompleted_task_with_tags():
     )
 
 
-@pytest.mark.skip(reason="TODO")
 def test_parse_multiple_tasks():
-    ...
+    raw: MarkdownStr = (
+        "- [ ] Do foo  #g:group1\n"
+        "- [ ] Do bar\n"
+        "  - Some details\n"
+        "- [ ] Do baz  #p:priority_a"
+    )
+    tasks = parse(raw)
+    assert tasks == [
+        Task(description="Do foo", completed=False, details=[], tags=["g:group1"]),
+        Task(description="Do bar", completed=False, details=["Some details"], tags=[]),
+        Task(description="Do baz", completed=False, details=[], tags=["p:priority_a"]),
+    ]
 
 
 @pytest.mark.skip(reason="TODO")
