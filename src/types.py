368874f53e7d43ca08740cc11c44747e4f4aba1a
David Torralba Goitia <david.torralba.goitia@gmail.com>
2021-09-01 13:27:57 +0100

    Parse tags

--- src/types.py
+++ src/types.py
@@ -1,13 +1,16 @@
+import re
 from dataclasses import dataclass
 from typing import List
 
 MarkdownStr = str
 TaskDetail = str
+TaskTag = str
 
 INCOMPLETE_TASK_PREFIX = "- [ ] "
 COMPLETED_TASK_PREFIX = "- [x] "
 DETAIL_PREFIX = "- "
 _NO_DETAILS: List[TaskDetail] = []
+TASK_TAG_PATTERN = re.compile(r"\s#([a-z]:[a-z0-9-_,]*)\s?")  # `#g:group1_b`
 
 
 @dataclass
@@ -15,6 +18,7 @@ class Task:
     description: str
     completed: bool
     details: List[TaskDetail]
+    tags: List[TaskTag]
 
 
 def parse(raw: MarkdownStr) -> Task:
@@ -31,16 +35,32 @@ def parse(raw: MarkdownStr) -> Task:
 
     if description.startswith(INCOMPLETE_TASK_PREFIX):
         description = description[6:]
-
-    if description.startswith(COMPLETED_TASK_PREFIX):
+    elif description.startswith(COMPLETED_TASK_PREFIX):
         description = description[6:]
         completed = True
+    else:
+        raise NotImplementedError(
+            "OOps, I didn't expect to reach this point. Variables in scope:"
+            f"\n{vars()}"
+        )
 
-    return Task(description=description, completed=completed, details=details)
+    tags = TASK_TAG_PATTERN.findall(description)
+
+    if tags:
+        # Remove tags from description
+        for tag in tags:
+            description = description.replace(f"#{tag}", "").strip()
+
+    return Task(
+        description=description,
+        completed=completed,
+        details=details,
+        tags=tags,
+    )
 
 
-def parse_details(raw: MarkdownStr) -> List[TaskDetail]:
-    lines = (line.strip() for line in raw.strip().split("\n"))
-    raw_details = (line for line in lines if line.startswith(DETAIL_PREFIX))
-    details = [raw_detail.replace(DETAIL_PREFIX, "") for raw_detail in raw_details]
+def parse_details(raw_details: MarkdownStr) -> List[TaskDetail]:
+    lines = (line.strip() for line in raw_details.strip().split("\n"))
+    raw_per_detail = (line for line in lines if line.startswith(DETAIL_PREFIX))
+    details = [raw_detail.replace(DETAIL_PREFIX, "") for raw_detail in raw_per_detail]
     return details
--- tests/test_types.py
+++ tests/test_types.py
@@ -6,13 +6,13 @@ from src.types import MarkdownStr, Task, parse
 def test_parse_a_single_uncompleted_task():
     raw: MarkdownStr = "- [ ] Do foo"
     task = parse(raw)
-    assert task == Task(description="Do foo", completed=False, details=[])
+    assert task == Task(description="Do foo", completed=False, details=[], tags=[])
 
 
 def test_parse_a_single_completed_task():
     raw: MarkdownStr = "- [x] Do foo"
     task = parse(raw)
-    assert task == Task(description="Do foo", completed=True, details=[])
+    assert task == Task(description="Do foo", completed=True, details=[], tags=[])
 
 
 def test_parse_a_single_uncompleted_task_with_details():
@@ -31,13 +31,19 @@ def test_parse_a_single_uncompleted_task_with_details():
             "details line 2",
             "details line 3",
         ],
+        tags=[],
     )
 
 
-@pytest.mark.skip(reason="TODO")
 def test_parse_a_single_uncompleted_task_with_tags():
-    raw: MarkdownStr = "- [x] Do foo  #g:group1"
-    assert raw
+    raw: MarkdownStr = "- [ ] Do foo  #g:group1"
+    task = parse(raw)
+    assert task == Task(
+        description="Do foo",  # do not show tags in the description
+        completed=False,
+        details=[],
+        tags=["g:group1"],
+    )
 
 
 @pytest.mark.skip(reason="TODO")
@@ -47,4 +53,17 @@ def test_parse_multiple_tasks():
 
 @pytest.mark.skip(reason="TODO")
 def test_parse_and_restore_tasks_in_the_right_shape():
+    ...
+
+
+@pytest.mark.skip(reason="TODO")
+def test_ignore_markdown_comments_in_task_details():
+    """This is not a hard constrain, but simplifies parsing logic.
+    Unless this feature is required do not complicate yourself.
+    """
+    ...
+
+
+@pytest.mark.skip(reason="TODO")
+def test_fail_validation_if_a_task_has_repeated_tags():
     ...
