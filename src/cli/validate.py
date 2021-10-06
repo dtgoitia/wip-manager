2851f0a7c253db64872e8f36d4e223497d31ddca
David Torralba Goitia <david.torralba.goitia@gmail.com>
2021-10-06 12:22:02 +0100

    Show diff on `validate`

--- src/cli/validate.py
+++ src/cli/validate.py
@@ -1,3 +1,4 @@
+import difflib
 from pathlib import Path
 
 from src.interpreter import items_to_markdown, parse_document
@@ -14,4 +15,19 @@ def validate_wip_file(*, path: Path, debug: bool) -> None:
         print(f"Parsed content dumped into {output_path}")
         output_path.write_text(parsed_content)
 
-    assert original_content == parsed_content
+    # Print diff
+    original_lines = original_content.split("\n")
+    parsed_lines = parsed_content.split("\n")
+    differ = difflib.Differ()
+    diff = differ.compare(original_lines, parsed_lines)
+
+    diffs_found = False
+    for i, line in enumerate(diff):
+        if line.startswith("  "):
+            continue
+        diffs_found = True
+        print(i, line)
+
+    if diffs_found:
+        # Exiting with error on diff is useful to concatenate CLI instructions
+        exit(1)
